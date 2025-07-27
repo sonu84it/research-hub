ca76705876a8aafef4950444cd289e050ccda691
aifirst <143290650+sonu84it@users.noreply.github.com>
2025-07-27 17:51:35 +0530

    Update BlogCard.jsx

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -27,7 +27,7 @@ export default function BlogCard({ title, summary, imageUrl, wikiUrl, podcastUrl
               rel="noopener noreferrer"
               className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors self-start"
             >
-              Padcast
+              Podcast
             </a>
           )}
         </div>
